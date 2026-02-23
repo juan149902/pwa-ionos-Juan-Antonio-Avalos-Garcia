3649be6301314bcfd1aa449ef261c1772766d79a
juan149902 <0322103675@ut-tijuana.edu.mx>
2026-02-23 11:59:46 -0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const completedTasksEl = document.getElementById('completed-tasks');
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
             .then(reg => console.log('✅ SW registrado:', reg.scope))
             .catch(err => console.error('❌ Error SW:', err));
     });
