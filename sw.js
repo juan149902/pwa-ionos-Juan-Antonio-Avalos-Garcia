e52fc65e216783816eb3201aae0b2f1e9603fa94
juan149902 <0322103675@ut-tijuana.edu.mx>
2026-02-23 11:38:26 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = 'task-manager-v1';
+const CACHE_NAME = 'task-manager-v2';
+
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/manifest.json',
-    '/icons/icon-192.png',
-    '/icons/icon-512.png'
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -19,28 +20,19 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches.keys()
-            .then(keys => Promise.all(
+        caches.keys().then(keys =>
+            Promise.all(
                 keys.filter(key => key !== CACHE_NAME)
                     .map(key => caches.delete(key))
-            ))
-            .then(() => self.clients.claim())
+            )
+        ).then(() => self.clients.claim())
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request)
-                .then(fetchResponse => {
-                    if (fetchResponse && fetchResponse.status === 200) {
-                        const clone = fetchResponse.clone();
-                        caches.open(CACHE_NAME)
-                            .then(cache => cache.put(event.request, clone));
-                    }
-                    return fetchResponse;
-                })
-            )
-            .catch(() => caches.match('/index.html'))
+            .then(response => response || fetch(event.request))
+            .catch(() => caches.match('./index.html'))
     );
 });
